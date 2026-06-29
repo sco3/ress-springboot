54ae1b5c7dc3a19b0c91534ce21169c3407a9bd7
Dmitry Zakharov <dzakarov@gmail.com>
2026-06-29 07:30:47 +0100

    fix: replace Jersey package scan with Spring classpath scan to fix fat jar scanning

--- ress-srv/src/main/java/sco/server/app/Application.java
+++ ress-srv/src/main/java/sco/server/app/Application.java
@@ -1,6 +1,10 @@
 package sco.server.app;
 
+import java.util.Set;
+
 import javax.annotation.PostConstruct;
+import javax.ws.rs.Path;
+import javax.ws.rs.ext.Provider;
 
 import org.glassfish.jersey.server.ResourceConfig;
 import org.glassfish.jersey.servlet.ServletContainer;
@@ -11,7 +15,9 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ClassPathScanningCandidateComponentProvider;
 import org.springframework.core.env.Environment;
+import org.springframework.core.type.filter.AnnotationTypeFilter;
 import com.fasterxml.jackson.databind.SerializationFeature;
 
 import sco.common.util.CasPropertiesHelper;
@@ -35,7 +41,21 @@ public class Application extends SpringBootServletInitializer {
     @Bean
     public ServletRegistrationBean jerseyServlet() {
         ResourceConfig config = new ResourceConfig();
-        config.packages(true, "sco.server.provider");
+
+        ClassPathScanningCandidateComponentProvider scanner =
+                new ClassPathScanningCandidateComponentProvider(false);
+        scanner.addIncludeFilter(new AnnotationTypeFilter(Path.class));
+        scanner.addIncludeFilter(new AnnotationTypeFilter(Provider.class));
+        Set<org.springframework.beans.factory.config.BeanDefinition> beans =
+                scanner.findCandidateComponents("sco.server.provider");
+        for (org.springframework.beans.factory.config.BeanDefinition bean : beans) {
+            try {
+                config.register(Class.forName(bean.getBeanClassName()));
+            } catch (ClassNotFoundException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
         config.property(SerializationFeature.FAIL_ON_EMPTY_BEANS.toString(), false);
 
         ServletRegistrationBean registration = new ServletRegistrationBean(
